27ffa846977e7877c2010ec12c3e0dfd9a9ac7a8
Pedro Filho <pedro@filho.me>
2020-03-11 13:43:48 +0000

    removing bloat

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,33 +1,35 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const srcDir = '../src/';
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const srcDir = "../src/";
 
 module.exports = {
-	entry: {
-		background: path.join(__dirname, srcDir + 'background.ts'),
-		options: path.join(__dirname, srcDir + 'options.ts')
-	},
-	output: {
-		path: path.join(__dirname, '../dist'),
-		filename: '[name].js'
-	},
-	optimization: {
-		splitChunks: {
-			name: 'vendor',
-			chunks: 'initial'
-		}
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/
-			}
-		]
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js']
-	},
-	plugins: [new CopyPlugin([{from: 'public/', to: '.', ignore: ['.DS_Store']}])]
+  entry: {
+    background: path.join(__dirname, srcDir + "background.ts"),
+    options: path.join(__dirname, srcDir + "options.ts")
+  },
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js"
+  },
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+      chunks: "initial"
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  plugins: [
+    new CopyPlugin([{ from: "public/", to: ".", ignore: [".DS_Store"] }])
+  ]
 };
